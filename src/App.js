ea1fe226b32396d98a533291b623f00c1f0f1ed5
Mau <qmau@SEAM3006487.local>
2016-07-20 10:22:05 -0700

    lowerdash has state set up. Make eventhandler to pass to children to change state

--- src/App.js
+++ src/App.js
@@ -35,8 +35,41 @@ class App extends React.Component {
         <div className="billboard bg-light">
           <h1>Data Visualization</h1>
         </div>
-        <LegComp data={this.state.c} yVal={this.state.cYVal} />
-        <ColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={this.state.cYVal} title={'This is a title'} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <DonutChart data={this.state.n} indy={'name'} dep={'population'} width={250} height={250} title={'Sales'}/>
+            </div>
+            <div className="col-md-4">
+              <ScatterPlot data={this.state.s} width={500} height={500} xVal={'x'} yVal={'y'} title={'This is a title'} />
+            </div>
+            <div className="col-md-4">
+              <StackedColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <ColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
+            </div>
+            <div className="col-md-4">
+              <StackedBarChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
+            </div>
+            <div className="col-md-4">
+              <BarChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <LineChart data={this.state.l} width={500} height={500} xVal={'date'} yVal={['usa']} title={'This is a title'} ticks={5}/>
+            </div>
+            <div className="col-md-4">
+              <BulletChart data={this.state.b} width={500} height={200} yVal={'id'} target={'target'} actual={'actual'} range={'range'} />
+            </div>
+            <div className="col-md-4">
+              <Card className="tile" name={'Growth'} des={'value in percent'} number={'19.1%'} data={this.state.l}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -45,40 +78,9 @@ class App extends React.Component {
 
 // <Legend data={this.state.c} width={100} height={100} dep={'name'} yVal={['freq1', 'freq2', 'freq3']} />
 // <Card2 className="tile" name={'Distribution'} des={'value in units'} number={'709'} data={this.state.c} />
-// <div className="container-fluid">
-//   <div className="row">
-//     <div className="col-md-4">
-//       <DonutChart data={this.state.n} indy={'name'} dep={'population'} width={250} height={250} title={'Sales'}/>
-//     </div>
-//     <div className="col-md-4">
-//       <ScatterPlot data={this.state.s} width={500} height={500} xVal={'x'} yVal={'y'} title={'This is a title'} />
-//     </div>
-//     <div className="col-md-4">
-//       <StackedColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
-//     </div>
-//   </div>
-//   <div className="row">
-//     <div className="col-md-4">
-//       <ColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
-//     </div>
-//     <div className="col-md-4">
-//       <StackedBarChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
-//     </div>
-//     <div className="col-md-4">
-//       <BarChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={['freq1', 'freq2']} title={'This is a title'} />
-//     </div>
-//   </div>
-//   <div className="row">
-//     <div className="col-md-4">
-//       <LineChart data={this.state.l} width={500} height={500} xVal={'date'} yVal={['usa']} title={'This is a title'} ticks={5}/>
-//     </div>
-//     <div className="col-md-4">
-//       <BulletChart data={this.state.b} width={500} height={200} yVal={'id'} target={'target'} actual={'actual'} range={'range'} />
-//     </div>
-//     <div className="col-md-4">
-//       <Card className="tile" name={'Growth'} des={'value in percent'} number={'19.1%'} data={this.state.l}/>
-//     </div>
-//   </div>
-// </div>
+
+
+// <LegComp data={this.state.c} yVal={this.state.cYVal} />
+// <ColumnChart data={this.state.c} width={500} height={500} xVal={'name'} yVal={this.state.cYVal} title={'This is a title'} />
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/Dashboard/LowerDash.js
+++ src/Dashboard/LowerDash.js
@@ -2,8 +2,22 @@ import React from 'react';
 //import legend Component
 
 class LowerDash extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    //initialize checks as empty, populate after
+    this.state = {
+      data: props.data,
+      currentData: props.data,
+      checks: [
+
+      ]
+    };
+    props.yVal.forEach(d => {
+      //push an object containing check id and its state (checked vs unchecked)
+      this.state.checks.push({name: d, isChecked: true});
+    });
+    console.log(this.state);
   }
 
   render() {
